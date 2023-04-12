217575e08bb5531d091bac7fc0934bdc93da73e1
Rahul M <roboto7o32oo3@gmail.com>
2023-04-13 00:34:06 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const HttpError = require('./utils/http-error');
+const eventRoutes = require('./routes/event.route');
+const _Event = require('./models/event.model');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
+app.use(bodyParser.json());
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
+app.use('/api/events', eventRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('could not find this route.', 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
+
+// connect to the mongoDB atlas database
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to mongoDB atlas database");
+
+    // open a change stream on the events collection
+    const changeStream = _Event.watch();
+
+    io.on('connection', (socket) => {
+      console.log('a user connected');
+
+      socket.emit('hello', "hello dude!");
+
+      changeStream.on('change', change => {
+        if (change.operationType === 'insert') {
+          socket.emit('newEvent', change.fullDocument);
+        }
+      });
+    });
+
+    server.listen(process.env.PORT, '0.0.0.0');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ models/event.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const eventSchema = mongoose.Schema({
+  timestamp: { type: String, required: true },
+  description: { type: String },
+  level: { type: Number, required: true },
+  image: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Event', eventSchema);
