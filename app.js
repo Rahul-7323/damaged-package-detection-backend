77702ee6fffc55ae74ec2fb9378a1af402db71ce
Rahul M <roboto7o32oo3@gmail.com>
2023-04-13 00:59:47 +0530

    added allow origin header

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   next();
 });
